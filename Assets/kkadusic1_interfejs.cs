448a0358b24000f6075962fb6402b74b69d266fa
kkadusic <kkadusic1@etf.unsa.ba>
2021-08-03 17:40:53 +0200

    Add Unity project

---
+++ Assets/kkadusic1_interfejs.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class kkadusic1_interfejs : MonoBehaviour
+{
+    public Text infoTekst;
+    public Text pomocTekst;
+
+    private bool prikazaneInfo = false;
+    private bool prikazanaPomoc = false;
+
+    public string url;
+    
+
+    public void izlaz()
+    {
+        Application.Quit();
+    }
+
+    public void informacije()
+    {
+        if (prikazaneInfo == false)
+        {
+            if (prikazanaPomoc)
+            {
+                pomocTekst.text = "";
+                prikazanaPomoc = false;
+            }
+            infoTekst.text = "TOWER BRIDGE AUGUMENTED REALITY" + Environment.NewLine +
+                        "Elektrotehnički fakultet Sarajevo" + Environment.NewLine +
+                        "Računarska grafika" + Environment.NewLine +
+                        "Kerim Kadušić" + Environment.NewLine +
+                        "Sarajevo, januar 2020.";
+            prikazaneInfo = true;
+        }
+        else
+        {
+            infoTekst.text = "";
+            prikazaneInfo = false;
+        }
+    }
+
+    public void pomoc()
+    {
+        if (prikazanaPomoc == false)
+        {
+            if (prikazaneInfo)
+            {
+                infoTekst.text = "";
+                prikazaneInfo = false;
+            }
+            pomocTekst.text = "UPUTE ZA RAD NAD APLIKACIJOM" + Environment.NewLine +
+                              "- Za prikaz videa pritisnuti na brod" + Environment.NewLine +
+                              "- Za prikaz osnovih informacija o mostu" + Environment.NewLine +
+                              "  pritisnuti na zastavu Velike Britanije" + Environment.NewLine +
+                              "- Za slusanje himne Velike Britanije" + Environment.NewLine +
+                              "  pritisnuti na crvenu telefonsku govorincu" + Environment.NewLine +
+                              " (Za zatvaranje neke od prethodnih stavki" + Environment.NewLine +
+                              " pritisnuti opet na neki od objekata)";
+            prikazanaPomoc = true;
+        }
+        else
+        {
+            pomocTekst.text = "";
+            prikazanaPomoc = false;
+        }
+    }
+
+    public void otvoriUrl()
+    {
+        Application.OpenURL(url);
+    }
+
+}
---
+++ Assets/kkadusic1_skripta.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class kkadusic1_skripta : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    public GameObject plane;
+    public GameObject tekst;
+    public GameObject audio;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            bool aktivanTekst = tekst.activeInHierarchy;
+            bool aktivanVideo = plane.activeInHierarchy;
+            bool aktivnaMuzika = audio.activeInHierarchy;
+
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name == "kkadusic1_Brodic")
+                {
+                    if (!aktivanVideo)
+                    {
+                        tekst.SetActive(false);
+                        audio.SetActive(false);
+                        plane.SetActive(true);
+                    }
+                    else
+                        plane.SetActive(false);
+                }
+                else if (hit.collider.gameObject.name == "kkadusic1_Zastava")
+                {
+                    if (!aktivnaMuzika)
+                    {
+                        plane.SetActive(false);
+                        tekst.SetActive(false);
+                        audio.SetActive(true);
+                    }
+                    else
+                        audio.SetActive(false);
+                }
+                else if (hit.collider.gameObject.name == "kkadusic1_Govornica")
+                {
+                    if (!aktivanTekst)
+                    {
+                        audio.SetActive(false);
+                        plane.SetActive(false);
+                        tekst.SetActive(true);
+                    }
+                    else
+                        tekst.SetActive(false);
+                }
+            }
+        }
+    }
+}
